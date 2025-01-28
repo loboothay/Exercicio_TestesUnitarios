1553939d22d5ae0f2d3478ddbd3a5fb0661637e0
Thaynara Lobo <loboo.thaynara@gmail.com>
2025-01-28 17:45:48 -0300

    feat: implementado os testes referentes aos exercício 6 e 7.

--- testesUnitarios/src/test/java/TestesUnitariosEasy.java
+++ testesUnitarios/src/test/java/TestesUnitariosEasy.java
@@ -1,7 +1,4 @@
-import easy.Exercicio02_easy;
-import easy.Exercicio03_easy;
-import easy.Exercicio04_easy;
-import easy.Exercicio05_easy;
+import easy.*;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -67,5 +64,64 @@ public class TestesUnitariosEasy {
         long terceiroValor = 6;
 
         Assert.assertEquals(72, exercicio05Easy.calcularMedia(primeiroValor, segundoValor, terceiroValor));
+    }
+
+    @Test
+    public void TesteUnitarioEx6_VerificaAlunoAprovado(){
+        Exercicio06_easy exercicio06Easy = new Exercicio06_easy();
+        double primeiraNota = 7;
+        double segundaNota = 6;
+        Assert.assertEquals("Você está aprovado!!!", exercicio06Easy.verificarAprovacaoAluno(primeiraNota, segundaNota));
+    }
+
+    @Test
+    public void TesteUnitarioEx6_VerificaAlunoDeExame(){
+        Exercicio06_easy exercicio06Easy = new Exercicio06_easy();
+        double primeiraNota = 5;
+        double segundaNota = 5;
+        Assert.assertEquals("Você está de exame!!!", exercicio06Easy.verificarAprovacaoAluno(primeiraNota, segundaNota));
+    }
+
+    @Test
+    public void TesteUnitarioEx6_VerificaAlunoReprovado(){
+        Exercicio06_easy exercicio06Easy = new Exercicio06_easy();
+        double primeiraNota = 5;
+        double segundaNota = 4;
+        Assert.assertEquals("Você está reprovado!!!", exercicio06Easy.verificarAprovacaoAluno(primeiraNota, segundaNota));
+    }
+
+    @Test
+    public void TesteUnitarioEx7_CalcularInssFaixa1() {
+        Exercicio07_easy exercicio07Easy = new Exercicio07_easy();
+        double resultado = exercicio07Easy.calcularInns(1300.00);
+        Assert.assertEquals(1300.00 * 0.075, resultado, 0.01);
+    }
+
+    @Test
+    public void TesteUnitarioEx7_CalcularInssFaixa2() {
+        Exercicio07_easy exercicio07Easy = new Exercicio07_easy();
+        double resultado = exercicio07Easy.calcularInns(2000.00);
+        Assert.assertEquals(2000.00 * 0.09, resultado, 0.01);
+    }
+
+    @Test
+    public void TesteUnitarioEx7_CalcularInssFaixa3() {
+        Exercicio07_easy exercicio07Easy = new Exercicio07_easy();
+        double resultado = exercicio07Easy.calcularInns(3000.00);
+        Assert.assertEquals(3000.00 * 0.12, resultado, 0.01);
+    }
+
+    @Test
+    public void TesteUnitarioEx7_CalcularInssFaixa4() {
+        Exercicio07_easy exercicio07Easy = new Exercicio07_easy();
+        double resultado = exercicio07Easy.calcularInns(4000.00);
+        Assert.assertEquals(4000.00 * 0.14, resultado, 0.01);
+    }
+
+    @Test
+    public void TesteUnitarioEx7_CalcularInssFaixa5() {
+        Exercicio07_easy exercicio07Easy = new Exercicio07_easy();
+        double resultado = exercicio07Easy.calcularInns(8000.00);
+        Assert.assertEquals(1051.04, resultado, 0.01);
     }
 }
